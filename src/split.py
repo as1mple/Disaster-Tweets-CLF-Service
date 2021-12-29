608e444bfc11ae1b6cee13dd97a7c19f34e25735
as1mple <netron61@gmail.com>
2021-12-29 12:55:12 +0200

    Add param shuffle to split data

--- src/split.py
+++ src/split.py
@@ -5,13 +5,14 @@ import pandas as pd
 
 if __name__ == "__main__":
     with open("./params.yaml", "r") as fd:
-        params = yaml.safe_load(fd)
+        params = yaml.safe_load(fd)['split']
 
     data = pd.read_csv("resources/train.csv")
     train, test = train_test_split(data,
                                    stratify=data.target.values,
-                                   test_size=params['split']['test_size'],
-                                   random_state=params['split']['seed'])
+                                   test_size=params['test_size'],
+                                   random_state=params['seed'],
+                                   shuffle=params['shuffle'])
 
     train.to_csv("resources/train_data.csv", index=False)
     test.to_csv("resources/test_data.csv", index=False)
