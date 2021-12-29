a70b2600ae2b45603ba8a4ee281576fa56836fb6
as1mple <netron61@gmail.com>
2021-12-29 12:45:58 +0200

    Add eval

--- README.md
+++ README.md
@@ -10,3 +10,5 @@
 > dvc run -n split -p split -d resources/train.csv -d src/split.py -o resources/train_data.csv -o resources/test_data.csv python src/split.py
 
 > dvc run -n train -p train -d resources/train_data.csv -d src/train.py -o resources/model.pkl python src/train.py
+
+> dvc run -n eval -p eval -d resources/test_data.csv -d resources/model.pkl -d src/eval.py -M metrics.json python src/eval.py
---
+++ src/eval.py
@@ -0,0 +1,30 @@
+import json
+import pickle
+
+import pandas as pd
+
+from modules.processing import piepline_preprocess
+from sklearn.metrics import f1_score
+
+if __name__ == "__main__":
+
+    test_data = pd.read_csv('resources/test_data.csv')
+    clf = pickle.load(open('resources/model.pkl', "rb"))
+
+    x_test = test_data.text.values
+    y_test = test_data.target.values
+
+    x_test = piepline_preprocess(x_test)
+    predict = clf.predict(x_test)
+    score = f1_score(y_test, predict)
+
+    with open("metrics.json", "w") as f:
+        json.dump(
+            {
+                "test": {
+                    "f1_score": score,
+                }
+            },
+            f,
+            indent=4,
+        )
